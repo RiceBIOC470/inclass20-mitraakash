b75b00ab9e549f6161b6969d5eebcd08fe592572
Mitra <amitra2@docmm0cl5001470.local>
2017-11-09 13:48:35 -0600

    Add all my files

--- Inclass20.m
+++ Inclass20.m
@@ -13,19 +13,19 @@ data = load('data.mat');
 fit_str = 'V*x/(K+x)';
 fitmodel = fittype(fit_str);
 
-[fit_out, fit_metric] = fit(xdat',ydat', fitmodel);
-plot(fit_out, xdat',ydat')
+[fit_out, fit_metric] = fit(xx',yy', fitmodel);
+plot(fit_out, xx',yy')
 
 fitmodel4 = fittype(fit_str, 'problem',{'K'});
-[fit_out4, fit_metric4] = fit(xdat',ydat', fitmodel4, 'problem', 100,'StartPoint', [10] );
-plot(fit_out4, xdat',ydat')
+[fit_out4, fit_metric4] = fit(xx',yy', fitmodel4, 'problem', 100,'StartPoint', [10] );
+plot(fit_out4, xx',yy')
 
-[fit_out2, fit_metric2] = fit(xdat',ydat2', fitmodel,'StartPoint', [10, 10]);
-plot(fit_out2, xdat',ydat2')
+[fit_out2, fit_metric2] = fit(xx',yy', fitmodel,'StartPoint', [10, 10]);
+plot(fit_out2, xx',yy')
 
 fitmodel3 = fittype(fit_str, 'problem','K');
-[fit_out3, fit_metric3] = fit(xdat',ydat2', fitmodel3, 'problem', 1000);
-plot(fit_out3, xdat',ydat2')
+[fit_out3, fit_metric3] = fit(xx',yy', fitmodel3, 'problem', 1000);
+plot(fit_out3, xx',yy')
 
 
 % 2. Use a custom model to try to fit the data to a Hill function with
@@ -33,8 +33,8 @@ plot(fit_out3, xdat',ydat2')
 
 fit_str2 = 'V*x^2/(K^2+x^2)';
 fitmodel5 = fittype(fit_str2);
-[fit_out5, fit_metric5] = fit(xdat',ydat', fitmodel5);
-plot(fit_out5, xdat',ydat')
+[fit_out5, fit_metric5] = fit(xx',yy', fitmodel5);
+plot(fit_out5, xx',yy')
 
 
 % 3. Now treat the exponent as a free parameter and perform the fit again,
@@ -43,16 +43,16 @@ plot(fit_out5, xdat',ydat')
 fit_str3 = 'V*x^n/(K^n+x^n)';
 fitmodel6 = fittype(fit_str3, 'problem',{'V','K'});
 coeffnames(fitmodel6)
-[fit_out6, fit_metric6] = fit(xdat',ydat',fitmodel6, 'problem', {1000000, 1000000});
-plot(fit_out6, xdat',ydat')
+[fit_out6, fit_metric6] = fit(xx',yy',fitmodel6, 'problem', {10, 10});
+plot(fit_out6, xx',yy')
 
 
 % 4. Redo parts 1 and 2, but using your model from 3 and fixing the value of
 % n to either 1 or 2.
 
-fitting_third_order1('V*x^1/(K^1+x^1)', xdat, ydat, ydat2);
-fitting_third_order1('V*x^2/(K^2+x^2)', xdat, ydat, ydat2);
+fitting_third_order1('V*x^1/(K^1+x^1)', xx, yy);
+fitting_third_order1('V*x^2/(K^2+x^2)', xx, yy);
 
-fitting_third_order2('V*x^1/(K^1+x^1)', xdat, ydat);
-fitting_third_order2('V*x^2/(K^2+x^2)', xdat, ydat);
+fitting_third_order2('V*x^1/(K^1+x^1)', xx, yy);
+fitting_third_order2('V*x^2/(K^2+x^2)', xx, yy);
 
--- fitting_third_order1.m
+++ fitting_third_order1.m
@@ -1,4 +1,4 @@
-function fitting_third_order1(fit_str, xdat, ydat, ydat2)
+function fitting_third_order1(fit_str, xdat, ydat)
 
 fitmodel = fittype(fit_str);
 
@@ -9,12 +9,12 @@ fitmodel4 = fittype(fit_str, 'problem',{'K'});
 [fit_out4, fit_metric4] = fit(xdat',ydat', fitmodel4, 'problem', 100,'StartPoint', [10] );
 plot(fit_out4, xdat',ydat')
 
-[fit_out2, fit_metric2] = fit(xdat',ydat2', fitmodel,'StartPoint', [10, 10]);
-plot(fit_out2, xdat',ydat2')
+[fit_out2, fit_metric2] = fit(xdat',ydat', fitmodel,'StartPoint', [10, 10]);
+plot(fit_out2, xdat',ydat')
 
 fitmodel3 = fittype(fit_str, 'problem','K');
-[fit_out3, fit_metric3] = fit(xdat',ydat2', fitmodel3, 'problem', 1000);
-plot(fit_out3, xdat',ydat2')
+[fit_out3, fit_metric3] = fit(xdat',ydat', fitmodel3, 'problem', 1000);
+plot(fit_out3, xdat',ydat')
 
 fitmodel5 = fittype(fit_str);
 [fit_out5, fit_metric5] = fit(xdat',ydat', fitmodel5);
